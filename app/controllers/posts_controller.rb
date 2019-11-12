42fc9e5a07720884fef3bb1d30f8284b8d14be47
PC-viejo <felipe97.11@gmail.com>
2019-11-11 21:46:43 -0500

    almost done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,4 +42,10 @@ class PostsController < ApplicationController
   def set_post
     @post = Post.find(params[:id])
   end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
 end
