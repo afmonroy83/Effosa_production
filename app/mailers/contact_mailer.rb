60ffffc1ae14f7755d5990f850445852055118a7
PC-viejo <felipe97.11@gmail.com>
2019-11-12 23:13:54 -0500

    fix?2

--- app/mailers/contact_mailer.rb
+++
@@ -1,7 +0,0 @@
-class ContactMailer < ApplicationMailer
-	def new_order_email
-    @contact = params[:contact]
-
-    mail(to: 'felipe97.11@gmail.com', subject: "Mensaje a traves de Effosa.com")
-  end
-end
