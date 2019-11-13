71f8e7d784faeeaeae270be46d4312e86c93bf87
PC-viejo <felipe97.11@gmail.com>
2019-11-12 23:19:22 -0500

    contactanos fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :groups
-  resources :contact, only: [:index, :new, :create]
- # get 'contactanos'    => 'contact/index', to: 'contact#index', as: "contactanos"
+  get 'contactanos'    => 'contact/index', to: 'contact#index', as: "contactanos"
   get 'nosotros'    =>'about/index', to: 'about#index', as: "nosotros"
   	devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
